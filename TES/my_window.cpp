9a113cbe97594cb0c2886601d0fc5a9bcc03ac75
DED .©-©- <arlndgeekovich@gmail.com>
2023-04-19 11:07:48 +0300

    Added coments and massive vector

--- TES/my_window.cpp
+++ TES/my_window.cpp
@@ -3,7 +3,8 @@
 #include <class_Stydent.h>
 
 #include <QGridLayout>
-
+#include <QVector>
+#include <vector>
 
 
 class Stydent{
@@ -14,6 +15,16 @@ private:
 
 
 public:
+//    Stydent(){
+//        last_name = "VECTOR_CLASS";
+//        number_group = 77777;
+
+
+//        Count_stydent_Object++;
+
+//        //qDebug() << "COUNSTRUCTOR STUDEBT";
+//    }
+
     Stydent(QString LN, int NG){
         last_name = LN;
         number_group = NG;
@@ -24,16 +35,22 @@ public:
         //qDebug() << "COUNSTRUCTOR STUDEBT";
     }
 
-    QString print(){
-        QTextStream out(stdout);
+    int print_count_objects(){
+        return Count_stydent_Object;
+    }
 
-        out << Count_stydent_Object;
+    QString print_last_name(){
+        return last_name;
+    }
+
+    int print_number_group(){
+        return number_group;
     }
 
 
     ~Stydent(){
         Count_stydent_Object--;
-        //qDebug() << "DESTRUCTOR STUDEBT";
+        //qDebug() << "DESTRUCTOR STUDENT";
     }
 
     static int Count_stydent_Object;
@@ -45,24 +62,28 @@ int Stydent::Count_stydent_Object = 0;
 
 
 
+
+
+class Student_butt : public Stydent{
+    public: QString print1() { return "VECTOR CLASS Student_butt"; }
+};
+
+
+
+
+
 Stydent a("Ukra", 2123), b("Ukra2", 2123), c("Ukra3", 2123);
+Stydent *d = new Stydent("qweqw", 123);
 
-Stydent rr("Alla", 422);
-Stydent r3("dasd", 477);
-Stydent rw("Ayerf", 2622);
-Stydent ry("53dsd", 252);
-Stydent rs("Asdg", 232);
+QVector <Stydent *> Stydents_massive;
+//Stydents_massive.append(new Stydent());
 
 
-
-
-
-
-
-
-
-
-
+//Stydent rr("Alla", 422);
+//Stydent r3("dasd", 477);
+//Stydent rw("Ayerf", 2622);
+//Stydent ry("53dsd", 252);
+//Stydent rs("Asdg", 232);
 
 
 
@@ -115,40 +136,42 @@ MY_window::MY_window(QWidget *parent)
 
     setLayout(Student_top_VBOX);
 
+
+    // Events
     connect(plsBtn, &QPushButton::clicked, this, &MY_window::set_new_valueToColumn);
     connect(mnsBtn, &QPushButton::clicked, this, &MY_window::remove_new_valueToColumn);
     connect(newTble, &QTableWidget::cellDoubleClicked, this, &MY_window::remove_click_new_valueToColumn);
 
 }
 
+// Slot for class creation
 void MY_window::set_new_valueToColumn(){
+    // Initializing streaming output
+    QTextStream out(stdout);
 
-
-    //newTble = new QTableWidget;
-    //newTble->setRowCount(5);
-    //newTble->setColumnCount(2);
-
-    //newCount = new QString;
-    //int localBuff = newCount->toInt();
-
-
-    QTextStream outt(stdout);
-
-
+    // Checking for a class destructor
     if(Stydent::Count_stydent_Object > 0){
 
-        newTble->setItem(0,0,new QTableWidgetItem(QString("ADDED")));
-        outt << "Cout objects: " << Stydent::Count_stydent_Object << Qt::endl;
 
-        Stydent rr("Alla", 422);
-        Stydent r3("dasd", 477);
-        Stydent rw("Ayerf", 2622);
-        Stydent ry("53dsd", 252);
-        Stydent rs("Asdg", 232);
+        // Setting a new value to a table
+        newTble->setItem(0,0,new QTableWidgetItem(QString(d->print_last_name())));
+        newTble->setItem(0,1,new QTableWidgetItem(QString::number(d->print_number_group())));
 
+        // Debug viewing objects
+        out << "Cout objects: " << Stydent::Count_stydent_Object << Qt::endl;
+
+        Stydents_massive.push_back(new Stydent("mmmmmm", 126));
+
+        out << "Cout objects: " << Stydent::Count_stydent_Object << Qt::endl;
     }
     //qDebug() << "+ | ";
 }
+
+
+
+
+
+
 
 void MY_window::remove_new_valueToColumn(){
     //newTble->setItem(0,0,new QTableWidgetItem(QString("")));
@@ -171,6 +194,7 @@ void MY_window::remove_click_new_valueToColumn(int row, int column){
 //    qDebug() << row;
 //    qDebug() << column;
 }
+
 
 
 
